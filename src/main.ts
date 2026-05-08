db81c5a1ac94cb375885c53563ec89b9fbf64cce
Mariana Soares <oliiveira.srs@gmail.com>
2026-05-08 11:59:43 -0300

    Swaggercerto

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,42 @@
-import { ValidationPipe, Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const logger = new Logger('Bootstrap'); // Cria um logger com o contexto "Bootstrap"
+  const app = await NestFactory.create(AppModule);
 
-  try {
-    const app = await NestFactory.create(AppModule);
+  // Configuração do CORS
+  app.enableCors({
+    origin: '*', // Em produção, coloque a URL do front-end
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
-    // Configuração do CORS
-    app.enableCors({
-      origin: '*', // Em produção, coloque a URL do front-end
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-    });
+  // Validation Pipe
+  app.useGlobalPipes(new ValidationPipe());
 
-    // Validation Pipe
-    app.useGlobalPipes(new ValidationPipe());
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Blog Pessoal')
+    .setDescription('Projeto Blog Pessoal')
+    .setVersion('1.0')
+    .setContact(
+      'Seu Nome',
+      'http://www.seugithub.com/',
+      'seuemail@email.com',
+    )
+    .addBearerAuth()
+    .build();
 
-    // Configuração do Swagger
-    const config = new DocumentBuilder()
-      .setTitle('Blog Pessoal')
-      .setDescription('Projeto Blog Pessoal')
-      .setVersion('1.0')
-      .setContact(
-        'Seu Nome',
-        'http://www.seugithub.com/',
-        'seuemail@email.com',
-      )
-      .addBearerAuth()
-      .build();
+  const document = SwaggerModule.createDocument(app, config);
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
-    // Configura o fuso horário
-    process.env.TZ = '-03:00';
+  process.env.TZ = '-03:00';
 
-    // Porta do Render ou Local
-    const port = process.env.PORT || 4000;
-
-    await app.listen(port);
-
-    // Se chegar aqui, significa que o AppModule carregou e o Banco conectou com sucesso
-    logger.log(✅ Servidor rodando em http://localhost:${port}/swagger);
-    logger.log(🐘 Conexão com o Banco de Dados PostgreSQL estabelecida com sucesso!);
-
-  } catch (error) {
-    // Caso ocorra erro na conexão com o banco ou inicialização
-    logger.error('❌ Falha ao iniciar a aplicação:', error);
-    process.exit(1); // Fecha o processo com erro
-  }
+  // Porta do Render
+  await app.listen(process.env.PORT || 4000);
 }
 
 bootstrap();
