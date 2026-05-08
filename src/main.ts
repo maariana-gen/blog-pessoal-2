a9f48b50805cb4874e570a45a1ab59d332797d12
Mariana Soares <oliiveira.srs@gmail.com>
2026-05-08 11:30:16 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,42 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Configuração do CORS
   app.enableCors({
-    origin: '', // Em produção, você pode substituir '' pela URL do seu front no Render
+    origin: '*', // Em produção, coloque a URL do front-end
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
+  // Validation Pipe
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 4000); // Importante para o Render usar a porta correta
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Blog Pessoal')
+    .setDescription('Projeto Blog Pessoal')
+    .setVersion('1.0')
+    .setContact(
+      'Seu Nome',
+      'http://www.seugithub.com/',
+      'seuemail@email.com',
+    )
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+
+  process.env.TZ = '-03:00';
+
+  // Porta do Render
+  await app.listen(process.env.PORT || 4000);
 }
+
 bootstrap();
