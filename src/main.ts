e835e2cb9727d85b093d7d6f483b595c9be7539c
Mariana Soares <oliiveira.srs@gmail.com>
2026-05-08 11:53:35 -0300

    Swaggerbraço

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { DataSource } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Configuração do CORS
   app.enableCors({
-    origin: '*',
+    origin: '*', // Em produção, coloque a URL do front-end
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
@@ -17,7 +16,7 @@ async function bootstrap() {
   // Validation Pipe
   app.useGlobalPipes(new ValidationPipe());
 
-  // Configuração Swagger
+  // Configuração do Swagger
   const config = new DocumentBuilder()
     .setTitle('Blog Pessoal')
     .setDescription('Projeto Blog Pessoal')
@@ -34,26 +33,10 @@ async function bootstrap() {
 
   SwaggerModule.setup('swagger', app, document);
 
-  // Timezone
   process.env.TZ = '-03:00';
 
-  // Verifica conexão com PostgreSQL
-  const dataSource = app.get(DataSource);
-
-  if (dataSource.isInitialized) {
-    console.log('PostgreSQL conectado com sucesso!');
-  } else {
-    console.log('Erro ao conectar no PostgreSQL!');
-  }
-
-  // Porta
-  const port = process.env.PORT || 4000;
-
-  // Inicializa servidor
-  await app.listen(port);
-
-  console.log(Servidor rodando na porta ${port});
-  console.log(Swagger disponível em /swagger);
+  // Porta do Render
+  await app.listen(process.env.PORT || 4000);
 }
 
 bootstrap();
